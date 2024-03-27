5ad5cf99f460bebca0374da505c00d22dc12a33d
Ricardo <ricardorosmaninhohenriques@gmail.com>
2024-03-27 08:58:19 +0100

    sixth commit

---
+++ something.js
@@ -0,0 +1,35 @@
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    const scrollThreshold = 300; // Le nombre de pixels de défilement avant de masquer le header
+
+    if (window.scrollY > scrollThreshold) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+});
+
+
+
+let lastScrollTop = 0;
+let scrollUpDistance = 0;
+const scrollThreshold = 300; // Modifiez ce seuil si nécessaire
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        // Défilement vers le bas
+        header.classList.add('hidden-2');
+        scrollUpDistance = 0;
+    } else {
+        // Défilement vers le haut
+        scrollUpDistance += (lastScrollTop - currentScroll);
+
+        if (scrollUpDistance >= scrollThreshold && header.classList.contains('hidden')) {
+            header.classList.remove('hidden-2');
+        }
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}, false);
