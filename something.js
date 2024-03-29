c70eac44a9dd337df3572f89260cb3142c390868
Ricardo <ricardorosmaninhohenriques@gmail.com>
2024-03-29 15:14:29 +0100

    9 commit

--- something.js
+++ something.js
@@ -1,19 +1,3 @@
-let lastScrollTop = 0; // Stocke la position précédente du défilement
-
-window.addEventListener("scroll", function() {
-    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScroll > lastScrollTop) {
-        // Défilement vers le bas
-        document.querySelector("header").style.top = "-100px"; // Modifiez cette valeur en fonction de la hauteur de votre en-tête
-    } else {
-        // Défilement vers le haut
-        document.querySelector("header").style.top = "0";
-    }
-
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Pour les navigateurs mobiles qui permettent de dépasser le scroll
-}, false);
-
 /* pour le bouton remonter */
 
 document.addEventListener("DOMContentLoaded", function() {
